df9e30bb35297af9891e3aece1b8dc87e72d8f63
Olivier Parent <olivpa@arteveldehs>
2020-02-11 16:22:01 +0100

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    'css': 'assets/css',
+    'images': 'assets/images'
+  });
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  return {
+    dir: {
+      input: 'views',
+      output: 'docs'
+    },
+    htmlTemplateEngine: 'njk',
+    pathPrefix: '/design-system-pgm/'
+  };
+};
