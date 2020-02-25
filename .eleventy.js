11e6311933d982efa332c633fa42f50ca005f115
Olivier Parent <olivpa@arteveldehs>
2020-02-25 09:02:15 +0100

    [WIP]

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,16 @@
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     'css': 'assets/css',
     'images': 'assets/images'
   });
+
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addWatchTarget("./css/");
+
   return {
     dir: {
       input: 'views',
